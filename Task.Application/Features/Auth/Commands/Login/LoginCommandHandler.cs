51c79441815c18d2d33df828fd71ff7d514926a3
Sametcl <samet2016.2022@gmail.com>
2025-08-20 22:05:43 +0300

    jwt ayarlari yapildi ve kullanici girisi sirasinda token degeri donduruldu .

---
+++ Task.Application/Features/Auth/Commands/Login/LoginCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Application.Interfaces.Token;
+using Task.Core.Entities;
+
+namespace Task.Application.Features.Auth.Commands.Login
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommandRequest, LoginCommandResponse>
+    {
+        private readonly SignInManager<AppUser> signInManager;
+        private readonly UserManager<AppUser> userManager;
+        private readonly ITokenService tokenService;
+
+        public LoginCommandHandler(SignInManager<AppUser> signInManager,
+        UserManager<AppUser> userManager,
+        ITokenService tokenService)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.tokenService = tokenService;
+        }
+        public async Task<LoginCommandResponse> Handle(LoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+                throw new UnauthorizedAccessException("Kullanıcı bulunamadı.");
+
+            var result = await signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            if (!result.Succeeded)
+                throw new UnauthorizedAccessException("Şifre yanlış.");
+
+            var token = await tokenService.CreateToken(user);
+            var response = new LoginCommandResponse
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = token.ValidTo
+            };
+            return response;
+        }
+    }
+}
