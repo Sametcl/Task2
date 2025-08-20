05c3bf0fca34f634def2e0fb0b21c7d56abed233
Sametcl <samet2016.2022@gmail.com>
2025-08-20 14:37:43 +0300

    dosyalar olusturuldu , entity ve identity user olusturuldu , context yapisi kuruldu ve ilk migration atildi .

---
+++ Task.Infrastructure/Persistance/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Task.Core.Entities;
+
+namespace Task.Infrastructure.Persistance.Context
+{
+    public class AppDbContext : IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Task.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Task.Core.Entities;
+using Task.Infrastructure.Persistance.Context;
+
+namespace Task.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+        }
+    }
+}
